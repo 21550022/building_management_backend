a51a87d7bfc41963edc831af6b1c6985c6913a8d
21550022 <21550022@gm.uit.edu.vn>
2024-09-05 19:48:55 +0700

    chore: Add TypeORM module and configure PostgreSQL database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from "typeorm";
+import { UsersModule } from './modules/users/users.module';
+import { User } from './modules/users/entities/user.entity';
+
+
+const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'mysecretpassword',
+  database: 'postgres',
+  entities: [User],
+  synchronize: true,
+}
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOptions),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
