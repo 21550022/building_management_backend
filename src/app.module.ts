47e885f4b7104875e4d903438cbf6d37f4fd5cc5
21550022 <21550022@gm.uit.edu.vn>
2024-09-09 14:23:34 +0700

    chore: Remove unused code and files related to users and categories modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,8 @@ import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
-import {UsersModule} from './modules/users/users.module';
 import {BuildingsModule} from './modules/buildings/buildings.module';
 import {BuildingLocationsModule} from './modules/building-locations/building-locations.module';
-import {CategoriesModule} from './modules/categories/categories.module';
 import {TraceIdMiddleware} from './middlewares/trace-id/trace-id.middleware';
 import {WinstonModule} from 'nest-winston';
 import {loggerOption} from './configs/logger.config';
@@ -17,10 +15,8 @@ import {dataSourceOptions} from './configs/database.config';
   imports: [
     WinstonModule.forRoot(loggerOption),
     TypeOrmModule.forRoot(dataSourceOptions),
-    UsersModule,
     BuildingsModule,
     BuildingLocationsModule,
-    CategoriesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
