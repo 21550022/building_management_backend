36fcc99e03d9cf014b75eb98b5f961aff8a93564
21550022 <21550022@gm.uit.edu.vn>
2024-09-09 10:37:48 +0700

    feat: implement logger to Buildings

--- src/modules/buildings/buildings.controller.ts
+++ src/modules/buildings/buildings.controller.ts
@@ -1,67 +1,93 @@
-import {Controller, Post, Get, Patch, Delete, Body, Param, ParseIntPipe, HttpStatus, InternalServerErrorException} from '@nestjs/common';
+import {Controller, Post, Get, Patch, Delete, Body, Param, ParseIntPipe, HttpStatus, InternalServerErrorException, Req} from '@nestjs/common';
 import {CreateBuildingDto} from './dto/create-building.dto';
 import {UpdateBuildingDto} from './dto/update-building.dto';
 import {BuildingsService} from './buildings.service';
 import {ApiResponseHandler} from 'src/common/response-handler';
+import {CustomLoggerService} from 'src/common/services/custom-logger/custom-logger.service';
+
 
 
 @Controller('buildings')
 export class BuildingsController {
-  constructor(private readonly buildingsService: BuildingsService) {}
+  constructor(
+    private readonly buildingsService: BuildingsService,
+    private readonly logger: CustomLoggerService,
+
+  ) {}
 
   @Post()
-  async create(@Body() createBuildingDto: CreateBuildingDto) {
+  async create(@Body() createBuildingDto: CreateBuildingDto, @Req() req: Request) {
+    const traceId = req.headers['x-trace-id'];
     try {
       const building = await this.buildingsService.create(createBuildingDto);
+      this.logger.log('Building created successfully');
       return ApiResponseHandler.created('Building created successfully', building);
     } catch (error) {
+      this.logger.error('Failed to create building', { traceId });
       throw ApiResponseHandler.error(new InternalServerErrorException('Failed to create building'));
     }
   }
 
   @Get()
-  async findAll() {
+  async findAll(@Req() req: Request) {
+    const traceId = req.headers['x-trace-id'];
     try {
       const buildings = await this.buildingsService.findAll();
+      this.logger.log('Buildings retrieved successfully');
       return ApiResponseHandler.ok('Buildings retrieved successfully', buildings);
     } catch (error) {
+      this.logger.error('Failed to retrieve buildings', { traceId });
       throw ApiResponseHandler.error(new InternalServerErrorException('Failed to retrieve buildings'));
     }
   }
 
   @Get(':id')
-  async findOne(@Param('id', ParseIntPipe) id: number) {
+  async findOne(@Param('id', ParseIntPipe) id: number, @Req() req: Request) {
+    const traceId = req.headers['x-trace-id'];
     try {
       const building = await this.buildingsService.findOne(id);
+      this.logger.log('Buildings retrieved successfully');
       return ApiResponseHandler.ok('Building retrieved successfully', building);
     } catch (error) {
       if (error.status === HttpStatus.NOT_FOUND) {
+        this.logger.error('Building not found', { traceId, buildingId: id });
         throw ApiResponseHandler.error(error);
       }
+      this.logger.error('Failed to retrieve building', { traceId, buildingId: id });
       throw ApiResponseHandler.error(new InternalServerErrorException('Failed to retrieve building'));
     }
   }
 
   @Patch(':id')
-  async update(@Param('id', ParseIntPipe) id: number, @Body() updateBuildingDto: UpdateBuildingDto) {
+  async update(@Param('id', ParseIntPipe) id: number, @Body() updateBuildingDto: UpdateBuildingDto, @Req() req: Request) {
+    const traceId = req.headers['x-trace-id'];
     try {
-      return await this.buildingsService.update(id, updateBuildingDto);
+      const building = await this.buildingsService.findOne(id);
+      this.logger.log('Building retrieved successfully');
+      return building;
     } catch (error) {
       if (error.status === HttpStatus.NOT_FOUND) {
+        this.logger.error('Building not found', { traceId, buildingId: id });
         throw ApiResponseHandler.error(error);
       }
+      this.logger.error('Failed to retrieve building', { traceId, buildingId: id });
       throw ApiResponseHandler.error(new InternalServerErrorException('Failed to update building'));
     }
   }
 
   @Delete(':id')
-  async remove(@Param('id', ParseIntPipe) id: number) {
+  async remove(@Param('id', ParseIntPipe) id: number, @Req() req: Request) {
+    const traceId = req.headers['x-trace-id'];
     try {
-      return await this.buildingsService.remove(id);
+      const ok = await this.buildingsService.findOne(id);
+      this.logger.log('Building retrieved successfully');
+      return ok;
     } catch (error) {
       if (error.status === HttpStatus.NOT_FOUND) {
+        this.logger.error('Building not found', { traceId, buildingId: id });
         throw ApiResponseHandler.error(error);
       }
+      this.logger.error('Failed to retrieve building', { traceId, buildingId: id });
       throw ApiResponseHandler.error(new InternalServerErrorException('Failed to delete building'));
     }
   }
