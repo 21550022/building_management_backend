5bbad6ae8213a69643e0cce9f55e8ca722a54f5f
21550022 <21550022@gm.uit.edu.vn>
2024-09-16 02:05:01 +0700

    Refactor logger.config.ts to use nest-winston module for logging

--- src/config/logger.config.ts
+++ src/config/logger.config.ts
@@ -1,37 +1,37 @@
 import { WinstonModuleOptions } from 'nest-winston';
-import { format, transports } from 'winston';
+import { format } from 'winston';
 import * as DailyRotateFile from 'winston-daily-rotate-file';
-import DatabaseStream from 'src/helpers/app-log/database-stream';
+import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
+
+
 
 const logFilePath = './logs/server-%DATE%.log';
 const errorLogFilePath = './logs/error-%DATE%.log';
 const logMaxSize = '128m';
 const logMaxCount = '14d';
 
-const formatTimestamp = format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' });
+const formatTimestamp = format.timestamp({ format: 'DD/MM/YYYY HH:mm:ss A' });
 
-const customFormat = format.printf(({ timestamp, level, message, ...meta }) => {
-  const metaString = JSON.stringify(meta);
-  return `${timestamp}\t${level.toUpperCase()}\t${message}\t${metaString}`;
-});
+const nestLikeFormat = nestWinstonModuleUtilities.format.nestLike('MyApp', {
+  colors: false,
+  prettyPrint: true,
+  processId: true,
+  appName: true,
+})
 
 export const loggerOption: WinstonModuleOptions = {
   level: 'info',
   format: format.combine(
-    format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
-    customFormat,
+    formatTimestamp,
+    format.ms(),
+    nestLikeFormat
   ),
   transports: [
-    new transports.Console({
-      format: format.combine(format.colorize({ all: true }), formatTimestamp),
-      level: 'info',
-    }),
     new DailyRotateFile({
       filename: logFilePath,
       datePattern: 'DD-MM-YYYY',
       maxSize: logMaxSize,
       maxFiles: logMaxCount,
-      format: format.combine(formatTimestamp, customFormat),
       level: 'info',
     }),
     new DailyRotateFile({
@@ -39,11 +39,10 @@ export const loggerOption: WinstonModuleOptions = {
       datePattern: 'DD-MM-YYYY',
       maxSize: logMaxSize,
       maxFiles: logMaxCount,
-      format: format.combine(formatTimestamp, customFormat),
       level: 'error',
     }),
-    new transports.Stream({
-      stream: new DatabaseStream(),
-    }),
+    // new transports.Stream({
+    //   stream: new DatabaseStream(),
+    // }),
   ],
 };
